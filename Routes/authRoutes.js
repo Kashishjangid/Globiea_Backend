caf1acc505609b6f06666876724d3af3afcbb256
Kashishjangid <kashishjangid71363@gmail.com>
2025-11-19 00:33:57 +0530

    Initial push

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../Controllers/authController');
 
 router.post('/signup', registerUser);
 router.post('/login', loginUser);
